c825831ed98ff93dc384621e227c1aef32f3df56
Leonardo <leo.fonseca@me.com>
2023-11-30 16:12:09 -0300

    Update android_app_installer.sh
    English version.

--- android_app_installer.sh
+++ android_app_installer.sh
@@ -1,173 +1,173 @@
 #!/bin/bash
-# Ferramenta de Instalação de Aplicações Android via ADB Wireless
-# Criado por Leonardo Fonseca
-# Licença: Licença MIT (Open Source)
+# Android Application Installation Tool via ADB Wireless
+# Created by Leonardo Fonseca
+# License: MIT License (Open Source)
 
-# Cores
+# Colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 CYAN='\033[0;36m'
-NC='\033[0m' # Sem Cor
+NC='\033[0m' # No Color
 
-# Obter o diretório do script
+# Get the script directory
 DIR=$(dirname "$(readlink -f "$0")")
 
-# URL para download do ADB
+# URL for downloading ADB
 ADB_ZIP_URL="https://dl.google.com/android/repository/platform-tools-latest-darwin.zip"
 ADB_ZIP_FILE="$DIR/platform-tools-latest-darwin.zip"
 ADB_DIR="$DIR/platform-tools"
 
-# Função para mostrar termos e condições
-exibir_termos_e_condicoes() {
+# Function to display terms and conditions
+display_terms_and_conditions() {
     clear
-    echo -e "${CYAN}Termos e Condições de Uso${NC}"
+    echo -e "${CYAN}Terms and Conditions of Use${NC}"
     echo "-------------------------"
-    echo "Este script utiliza o Android Debug Bridge (ADB) para instalar aplicativos."
-    echo "Para mais informações sobre como ativar o modo debug em seu dispositivo, visite:"
-    echo "https://developer.android.com/studio/command-line/adb?hl=pt-br"
-    echo -e "\nAVISO LEGAL: Este software é fornecido 'como está', sem garantia de qualquer tipo. O uso é por conta e risco do utilizador."
-    echo "O criador deste script não se responsabiliza por quaisquer danos resultantes do uso deste software."
-    echo -e "\nPressione Enter para aceitar os termos e condições e continuar ou qualquer outra tecla para sair."
-    read -s -n 1 aceitacao
-    if [ "$aceitacao" != "" ]; then
-        echo -e "${RED}Você escolheu sair. Os termos e condições não foram aceitos.${NC}"
+    echo "This script uses Android Debug Bridge (ADB) to install applications."
+    echo "For more information on enabling debug mode on your device, visit:"
+    echo "https://developer.android.com/studio/command-line/adb?hl=en"
+    echo -e "\nDISCLAIMER: This software is provided 'as is', without any kind of warranty. Use is at the user's own risk."
+    echo "The creator of this script is not responsible for any damages resulting from the use of this software."
+    echo -e "\nPress Enter to accept the terms and conditions and continue or any other key to exit."
+    read -s -n 1 acceptance
+    if [ "$acceptance" != "" ]; then
+        echo -e "${RED}You have chosen to exit. Terms and conditions were not accepted.${NC}"
         exit 4
     fi
 }
 
-# Função para contar APKs no diretório
-contar_apks() {
+# Function to count APKs in the directory
+count_apks() {
     find "$1" -maxdepth 1 -name "*.apk" | wc -l
 }
 
-# Verificar se há APKs na pasta apk
+# Check if there are APKs in the apk folder
 APK_DIR="$DIR/apk"
-if [ $(contar_apks "$APK_DIR") -eq 0 ]; then
-    echo -e "${RED}Nenhum APK foi encontrado na pasta 'apk'.${NC}"
+if [ $(count_apks "$APK_DIR") -eq 0 ]; then
+    echo -e "${RED}No APK found in the 'apk' folder.${NC}"
     exit 5
 fi
 
-# Função para verificar e baixar o ADB
+# Function to check and download ADB
 download_adb() {
     if [ ! -f "$ADB_DIR/adb" ]; then
-        echo -e "${YELLOW}ADB não encontrado. Deseja baixar o ADB agora? (y/n):${NC}"
+        echo -e "${YELLOW}ADB not found. Would you like to download ADB now? (y/n):${NC}"
         read -r resp
         if [[ $resp == "y" ]]; then
-            echo "Baixando ADB para macOS..."
+            echo "Downloading ADB for macOS..."
             curl -L -o "$ADB_ZIP_FILE" "$ADB_ZIP_URL" --progress-bar && {
                 unzip -q "$ADB_ZIP_FILE" -d "$DIR" && {
                     chmod +x "$ADB_DIR/adb"
-                    echo -e "${GREEN}ADB baixado e configurado com sucesso.${NC}"
+                    echo -e "${GREEN}ADB successfully downloaded and configured.${NC}"
                 } || {
-                    echo -e "${RED}Falha ao descompactar o ADB.${NC}"
+                    echo -e "${RED}Failed to unzip ADB.${NC}"
                     exit 2
                 }
             } || {
-                echo -e "${RED}Erro ao baixar o ADB.${NC}"
+                echo -e "${RED}Error downloading ADB.${NC}"
                 exit 1
             }
         fi
     fi
 }
 
-# Função para conectar ao dispositivo
-conectar_dispositivo() {
-    local ip_dispositivo
-    local nome_dispositivo
+# Function to connect to the device
+connect_device() {
+    local device_ip
+    local device_name
 
-    # Verificar se o arquivo de última conexão existe
+    # Check if the last connection file exists
     if [ -f "$DIR/last_connection.txt" ]; then
-        read -r ip_dispositivo nome_dispositivo < "$DIR/last_connection.txt"
-        echo "Última conexão foi com o dispositivo '$nome_dispositivo' com IP $ip_dispositivo."
-        echo -e "${YELLOW}Deseja utilizar os dados da última conexão? (y/n):${NC}"
+        read -r device_ip device_name < "$DIR/last_connection.txt"
+        echo "Last connection was with the device '$device_name' with IP $device_ip."
+        echo -e "${YELLOW}Do you want to use the data from the last connection? (y/n):${NC}"
         read resp
         if [[ $resp != "y" ]]; then
-            echo -e "${YELLOW}Digite o IP do dispositivo:${NC}"
-            read ip_dispositivo
-            nome_dispositivo=""  # Resetar o nome do dispositivo, pois será obtido novamente
+            echo -e "${YELLOW}Enter the device IP:${NC}"
+            read device_ip
+            device_name=""  # Reset the device name, as it will be obtained again
         fi
     else
-        echo "Nenhuma conexão anterior encontrada."
-        echo -e "${YELLOW}Digite o IP do dispositivo:${NC}"
-        read ip_dispositivo
+        echo "No previous connection found."
+        echo -e "${YELLOW}Enter the device IP:${NC}"
+        read device_ip
     fi
 
-    if ! "$ADB_DIR/adb" connect "$ip_dispositivo"; then
-        echo -e "${RED}Falha ao conectar ao dispositivo. Verifique a conexão e o IP inserido.${NC}"
+    if ! "$ADB_DIR/adb" connect "$device_ip"; then
+        echo -e "${RED}Failed to connect to the device. Check the connection and the IP entered.${NC}"
         exit 3
     fi
 
-    # Verificar se a conexão foi bem-sucedida antes de prosseguir
-    if ! "$ADB_DIR/adb" -s "$ip_dispositivo" shell echo "conexão bem-sucedida" &> /dev/null; then
-        echo -e "${RED}Falha ao estabelecer uma conexão estável com o dispositivo.${NC}"
+    # Check if the connection was successful before proceeding
+    if ! "$ADB_DIR/adb" -s "$device_ip" shell echo "successful connection" &> /dev/null; then
+        echo -e "${RED}Failed to establish a stable connection with the device.${NC}"
         exit 4
     fi
 
-    if [ -z "$nome_dispositivo" ]; then
-        nome_dispositivo=$("$ADB_DIR/adb" -s "$ip_dispositivo" shell getprop ro.product.model)
+    if [ -z "$device_name" ]; then
+        device_name=$("$ADB_DIR/adb" -s "$device_ip" shell getprop ro.product.model)
     fi
-    echo "$ip_dispositivo $nome_dispositivo" > "$DIR/last_connection.txt"
-    echo -e "${GREEN}Conectado com sucesso ao dispositivo '$nome_dispositivo' com IP $ip_dispositivo.${NC}"
+    echo "$device_ip $device_name" > "$DIR/last_connection.txt"
+    echo -e "${GREEN}Successfully connected to the device '$device_name' with IP $device_ip.${NC}"
 }
 
-# Função para listar APKs encontrados
-listar_apks_encontrados() {
-    if [ $(contar_apks "$APK_DIR") -gt 0 ]; then
-        echo -e "${CYAN}Aplicativos encontrados para instalação:${NC}"
+# Function to list APKs found
+list_found_apks() {
+    if [ $(count_apks "$APK_DIR") -gt 0 ]; then
+        echo -e "${CYAN}Applications found for installation:${NC}"
         for apk in "$APK_DIR"/*.apk; do
             echo "$(basename "$apk")"
         done
     fi
 }
 
-# Função para listar e instalar APKs
-listar_e_instalar_apks() {
-    INSTALADOS_DIR="$APK_DIR/instalados"
-    [ ! -d "$INSTALADOS_DIR" ] && mkdir -p "$INSTALADOS_DIR"
+# Function to list and install APKs
+list_and_install_apks() {
+    INSTALLED_DIR="$APK_DIR/installed"
+    [ ! -d "$INSTALLED_DIR" ] && mkdir -p "$INSTALLED_DIR"
 
-    listar_apks_encontrados
+    list_found_apks
 
     for apk in "$APK_DIR"/*.apk; do
-        apk_nome="$(basename "$apk")"
-        echo -e "${YELLOW}Deseja instalar $apk_nome no dispositivo? (y/n):${NC}"
+        apk_name="$(basename "$apk")"
+        echo -e "${YELLOW}Do you want to install $apk_name on the device? (y/n):${NC}"
         read -r resp
         if [[ $resp == "y" ]]; then
-            "$ADB_DIR/adb" -s "$ip_dispositivo" install -r "$apk" && {
-                mv "$apk" "$INSTALADOS_DIR"
-                echo -e "${GREEN}$apk_nome instalado com sucesso.${NC}"
+            "$ADB_DIR/adb" -s "$device_ip" install -r "$apk" && {
+                mv "$apk" "$INSTALLED_DIR"
+                echo -e "${GREEN}$apk_name successfully installed.${NC}"
             } || {
-                echo -e "${RED}Falha ao instalar $apk_nome.${NC}"
+                echo -e "${RED}Failed to install $apk_name.${NC}"
             }
         fi
     done
 
-    echo -e "${CYAN}Resumo da instalação:${NC}"
-    for apk in "$INSTALADOS_DIR"/*.apk; do
-        apk_nome="$(basename "$apk")"
-        echo "$apk_nome: Instalado"
+    echo -e "${CYAN}Installation summary:${NC}"
+    for apk in "$INSTALLED_DIR"/*.apk; do
+        apk_name="$(basename "$apk")"
+        echo "$apk_name: Installed"
     done
-    if [ $(contar_apks "$INSTALADOS_DIR") -eq 0 ]; then
-        echo "Nenhum APK foi instalado."
+    if [ $(count_apks "$INSTALLED_DIR") -eq 0 ]; then
+        echo "No APK was installed."
     fi
 }
 
-desconectar_dispositivo() {
-    local ip_dispositivo="$1"
+disconnect_device() {
+    local device_ip="$1"
 
-    if [ -z "$ip_dispositivo" ]; then
-        echo "Desconectando todos os dispositivos..."
+    if [ -z "$device_ip" ]; then
+        echo "Disconnecting all devices..."
         "$ADB_DIR/adb" disconnect
     else
-        echo "Desconectando o dispositivo com IP $ip_dispositivo..."
-        "$ADB_DIR/adb" disconnect "$ip_dispositivo"
+        echo "Disconnecting the device with IP $device_ip..."
+        "$ADB_DIR/adb" disconnect "$device_ip"
     fi
 }
 
-# Início do script
-exibir_termos_e_condicoes
+# Start of the script
+display_terms_and_conditions
 download_adb
-conectar_dispositivo
-listar_e_instalar_apks
-desconectar_dispositivo
+connect_device
+list_and_install_apks
+disconnect_device
